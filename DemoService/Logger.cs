8b517b991385e7636a2141c60445655169e88829
Anamika Sawhney <anamika.sawhney22@gmail.com>
2019-04-16 21:19:01 +0530

    Add project files.

---
+++ DemoService/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoService
+{
+   public static class Logger
+    {
+        public static void WriteToLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                string Date = System.DateTime.Now.ToString("dd-mm-yyyy");
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFolder\\DemoService" + Date + ".txt", true);
+                sw.Write(DateTime.Now.ToString() + " : " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex) { }
+            }
+    }
+}
---
+++ DemoService/Service1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DemoService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        Timer timer = new Timer();
+        protected override void OnStart(string[] args)
+        {
+            timer.Interval = 1000;
+            timer.Elapsed += new ElapsedEventHandler(timer_tick);
+            this.timer.Enabled = true;
+            Logger.WriteToLog("Demo Service started .... ");
+        }
+
+        private void timer_tick(object sender, ElapsedEventArgs e)
+        {
+
+            Logger.WriteToLog("Timer ticked .... ");
+            Logger.WriteToLog("Successful");
+        }
+    
+
+        protected override void OnStop()
+        {
+            Logger.WriteToLog("Service is about to close");
+            timer.Stop();
+            timer = null;
+            Logger.WriteToLog("Service is shutdown by user");
+        }
+    }
+}
